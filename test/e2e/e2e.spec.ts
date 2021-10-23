6e83329eb4d5ee051e7f51eb695e13fb79917fc2
sxlwar <sxlwar@163.com>
2021-10-23 14:44:24 +0800

    2

--- test/e2e/e2e.spec.ts
+++ test/e2e/e2e.spec.ts
@@ -4,7 +4,7 @@ import path from 'path';
 const { Application } = require('spectron');
 
 const delay = (time: number) => new Promise(resolve => setTimeout(resolve, time));
-// 1
+// 1 -> 2
 
 describe('main window', function spec() {
   let app: any;
